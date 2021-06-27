3f7529f4a30c01b22806f104f9b350ec714ce2f6
Paolo Chiabrera <paolo.chiabrera@glovoapp.com>
2021-06-27 13:55:07 +0200

    feat: use only faker

--- routes/random.js
+++ routes/random.js
@@ -1,32 +1,18 @@
 const express = require('express');
 
-const Moniker = require('moniker');
-const capitalize = require('lodash.capitalize');
 const faker = require('faker');
 const zipcodes = require('zipcodes');
 
 const router = express.Router();
 
-const getRandomData = () => {
-  let username = Moniker.choose();
-  let pwd = Moniker.choose();
-
-  try {
-    username = username.split('-').map(capitalize).join('');
-    pwd = pwd.split('-').map(capitalize).join('');
-  } catch (e) {
-    console.error('Username manipulation', e);
-  }
-
-  return {
-    email: faker.internet.email().toLowerCase(),
-    name: faker.name.firstName(),
-    pwd,
-    surname: faker.name.lastName(),
-    username,
-    zipcode: zipcodes.random(),
-  };
-};
+const getRandomData = () => ({
+  email: faker.internet.email().toLowerCase(),
+  name: faker.name.firstName(),
+  pwd: faker.internet.password(),
+  surname: faker.name.lastName(),
+  username: faker.internet.userName(),
+  zipcode: zipcodes.random(),
+});
 
 router.get('/random', (req, res) => {
   res.json(getRandomData());
